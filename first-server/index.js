0b51391618fa35e20cd07f9980475cb4fc18242b
yogeshweb-dev <yogeshkumar1668074@gmail.com>
2025-06-21 11:10:27 +0530

    bssic api in node js

--- first-server/index.js
+++ first-server/index.js
@@ -3,3 +3,15 @@ var b = 20;
 var c = a+b;
 
 console.log(c)
+
+// cmd main output dekane ke liye
+
+// 1- node index.js
+// 2- nodemon
+// 3- nodemon index.js
+
+
+// package.json : file creat karne ke liye
+
+// 1- npm init
+// 2- mpm init -y
--- first-server/server.js
+++ first-server/server.js
@@ -1,7 +1,71 @@
 const http = require('http')
+const { json } = require('stream/consumers')
+const { categories, brands, product } = require('./data')
 
-http.createServer((request, resopnse) => {
-    console.log('hello world')
+const server = http.createServer((request , response) => {
+    console.log(request.url)
+    console.log(request.method)
 
-    resopnse.end('my name is yogesh kumar')
-}).listen(2000)
+    if(request.url == '/'){
+         response.end('<h1> This is a home page </h1>')
+    }else if(request.url == '/categories'){
+
+        if(categories.length > 0){
+             const result = {
+            '_status' : true,
+            '_massage' : 'Categories add Successfully',
+            '_data': categories
+        }
+         response.end(JSON.stringify(result));
+        }else{
+             const result = {
+            '_status' : false,
+            '_massage' : 'No Categories add',
+            '_data': categories
+        }
+         response.end(JSON.stringify(result));
+        }
+       
+    }else if(request.url == '/brands'){
+
+        if(brands.length > 0){
+            const result = {
+                '_status' : true,
+                '_massage' : 'brand add Successfully',
+                '_data' : brands
+            }
+            response.end(JSON.stringify(result))
+        }else{
+             const result = {
+                '_status' : false,
+                '_massage' : ' No brand add',
+                '_data' : brands
+            }
+            response.end(JSON.stringify(result))
+        }
+
+    }else if(request.url == '/product'){
+        if(product.length > 0){
+            const result = {
+                "_status" : true,
+                '_massage' : 'Product add Successfull !!',
+                '_data' : product
+            }
+            response.end(JSON.stringify(result))
+        }else{
+            const result = {
+                '_status' : false,
+                '_massage' : 'Not product add !!!',
+                '_data' : product
+            }
+            response.end(JSON.stringify(result))
+        }
+
+    }else{
+         response.end('<h1> 404 Page not fond ! ... </h1>')
+    }
+})
+
+server.listen(7000,() => {
+    console.log('server is working fine ! ...')
+})
